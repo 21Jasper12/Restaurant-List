ad741c96a0bc2312cc03ea0776fbc41ed0fc6421
21Jasper <pickeng2389002@gmail.com>
2022-02-19 16:42:44 +0800

    新增handlebars參數、bootstrap(CSS、JS)、Popper.js和fontawesome.css

--- app.js
+++ app.js
@@ -3,9 +3,15 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+// 設定handlebars
+const exphbs = require('express-handlebars')
+
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
+app.set('view engine', 'handlebars')
+
 // 設定路由
 app.get('/', (req, res) => {
-  res.send(`<h1>TEST TEST</h1>`)
+  res.render('index')
 })
 
 // 啟動與監聽路由
